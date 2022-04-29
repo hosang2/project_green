883da78e82b7b5e8516a2cd4068aa4042084efbe
97Fekim <rlatmdcjf9@gmail.com>
2022-04-29 12:13:48 +0900

    Initial commit

---
+++ src/test/java/com/group3/project_green/ProjectGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.project_green;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
