26a95b319f2f46df96fa7f2a36b30f24864f1508
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-08 23:01:52 +0900

    FEAT:m FEAT 채팅기능 정상동작 확인(controller, html)

--- src/main/java/com/group3/project_green/controller/ChatController.java
+++ src/main/java/com/group3/project_green/controller/ChatController.java
@@ -1,2 +1,16 @@
-package com.group3.project_green.controller;public class ChatController {
+package com.group3.project_green.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    @GetMapping("/room")
+    public void goChat(){
+
+    }
+
 }
