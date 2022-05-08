535ffa7d5dba7d501321aef5177d9b8af54c5d94
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-08 23:00:43 +0900

    FEAT: 멤버 엔티티 생성

---
+++ src/main/java/com/group3/project_green/controller/ChatController.java
@@ -0,0 +1,2 @@
+package com.group3.project_green.controller;public class ChatController {
+}
--- src/main/java/com/group3/project_green/entity/Member.java
+++ src/main/java/com/group3/project_green/entity/Member.java
@@ -1,2 +1,25 @@
-package com.group3.project_green.entity;public class Member {
+package com.group3.project_green.entity;
+
+import com.group3.project_green.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
 }
