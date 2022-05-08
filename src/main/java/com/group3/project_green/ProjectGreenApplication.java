c47e4e1f160b28e939bb1d027d06de1669fb1059
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-08 22:47:49 +0900

    DOCS: BaseEntity auditing 설정

--- src/main/java/com/group3/project_green/ProjectGreenApplication.java
+++ src/main/java/com/group3/project_green/ProjectGreenApplication.java
@@ -2,7 +2,9 @@ package com.group3.project_green;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProjectGreenApplication {
 
