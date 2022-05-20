4e529dbbe99870b99c827d7d838175615ffebfc6
present322 <present322@naver.com>
2022-05-20 12:22:28 +0900

    Test MemberDTO

--- src/main/java/com/group3/project_green/DTO/MemberDTO.java
+++ src/main/java/com/group3/project_green/DTO/MemberDTO.java
@@ -1,7 +1,19 @@
 package com.group3.project_green.DTO;
 
+import com.group3.project_green.entity.Accom;
+import com.group3.project_green.entity.Food;
+import com.group3.project_green.entity.Member;
+import com.group3.project_green.entity.Sights;
+import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
+@Builder
 public class MemberDTO {
+    private Long id;
+    private String password;
+    private String email;
+    private LocalDateTime regDate,modDate;
 }
