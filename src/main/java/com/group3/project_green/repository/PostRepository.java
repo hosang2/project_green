c30fd8c3870914b3da82be424321f76954c6fa5f
leejungkuk <siro5643@gmail.com>
2022-05-21 20:22:56 +0900

    FEAT: comment 조회 구현, 삭제 진행중

--- src/main/java/com/group3/project_green/repository/PostRepository.java
+++ src/main/java/com/group3/project_green/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.group3.project_green.repository;
 import com.group3.project_green.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
