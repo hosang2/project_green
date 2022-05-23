fb2621a4342b59a155adb3496f42b1af1a89d65f
Administrator <Administrator@User -2022RLNHX>
2022-05-23 15:34:52 +0900

    TEST: test

--- src/main/java/com/group3/project_green/controller/ListController.java
+++ src/main/java/com/group3/project_green/controller/ListController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import java.util.List;
 public class ListController {
 
     private final PostService postService;
+    private final PostRepository postRepository;
 
 //    @GetMapping("/")
 //    public String goChat(){
@@ -30,32 +32,32 @@ public class ListController {
 
         return "/home/list";
     }
-    @PostMapping("/food")
+    @GetMapping("/food")
     public String gofood(Model model ){
-
+        model.addAttribute("post",postService.getFoodList());
         // 모델에 음식에 관련된 리스트를 실어서 보냅니다
 
         return "/home/list";
     }
 
-    @GetMapping("/landmark")
-    public String goRead(){
-
+    @GetMapping("/sight")
+    public String goSight(Model model){
+        model.addAttribute("post",postService.getSightsList());
         // 모델에 관광지 관련된 리스트를 실어서 보냅니다.
         return "/home/list";
     }
 
-    @GetMapping("acom")
-    public String goAcom(){
-
+    @GetMapping("accom")
+    public String goAccom(Model model){
+        model.addAttribute("post",postService.getAccomList());
         //모델에 숙박과 관려된 리스트를 실어서 보냅니다..
         return "/home/list";
     }
 
     @GetMapping("/read")
-    public String read() {
-
-        return "/home/read";
+    public void read(Long pno, Model model) {
+        Post result = postRepository.getById(pno);
+        model.addAttribute("result", result);
     }
     @GetMapping("/insert")
     public String insert() {
@@ -74,6 +76,10 @@ public class ListController {
         return "/home/userinfo";
     }
 
+    @GetMapping ("/memberPostList")
+    public String memberPostList(){
 
-
+        return "/home/memberPostList";
+    }
 }
+
--- src/test/java/com/group3/project_green/PostTests.java
+++ src/test/java/com/group3/project_green/PostTests.java
@@ -1,10 +1,10 @@
 package com.group3.project_green;
 
-import com.group3.project_green.entity.Food;
-import com.group3.project_green.entity.Member;
-import com.group3.project_green.entity.Post;
+import com.group3.project_green.entity.*;
+import com.group3.project_green.repository.AccomRepository;
 import com.group3.project_green.repository.FoodRepository;
 import com.group3.project_green.repository.PostRepository;
+import com.group3.project_green.repository.SightRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,27 +21,43 @@ public class PostTests {
     @Autowired
     private FoodRepository foodRepository;
 
+    @Autowired
+    private AccomRepository accomRepository;
+    @Autowired
+    private SightRepository sightRepository;
+
+
     @Test
     void insertDummy(){
 
-//        for(int i=0; i<20; i++){
+//        for(int i=0; i<6; i++){
 //
-//            Food food1 = Food.builder()
-//                    .name("음식" + i)
-//                    .category("음식종류" + i)
+//            Sights sights = Sights.builder()
+//                    .name("관광지"+i)
+//                    .category("관광지종류"+i)
 //                    .build();
 //
-//            foodRepository.save(food1);
+//            sightRepository.save(sights);
 //
 //        }
 
+//        for(int i=0; i<3; i++){
+//
+//            Accom accom =Accom.builder()
+//                    .name("숙소"+i)
+//                    .category("숙소"+i)
+//                    .build();
+//
+//            accomRepository.save(accom);
+//
+//        }
 
-        for(int i=2; i<=21; ++i){
+        for(int i=1; i<=6; ++i){
 
-            Optional<Food> temp = foodRepository.findById(new Long(i));
+            Optional<Sights> temp = sightRepository.findById(new Long(i));
 
             Post post1 = Post.builder()
-                    .food(temp.get())
+                    .sights(temp.get())
                     .title("제목"+i)
                     .likeNum(new Long(i))
                     .content("내용"+i)
