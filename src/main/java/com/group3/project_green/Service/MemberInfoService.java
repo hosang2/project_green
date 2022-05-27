0d8327dffc8a6c31bc587a2037efb9e085bc9567
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-27 22:06:01 +0900

    FEAT: 회원정보 Service 계층 생성

--- src/main/java/com/group3/project_green/Service/MemberInfoService.java
+++ src/main/java/com/group3/project_green/Service/MemberInfoService.java
@@ -1,2 +1,33 @@
-package com.group3.project_green.Service;public interface MemberInfoService {
+package com.group3.project_green.Service;
+
+import com.group3.project_green.DTO.MemberDTO;
+import com.group3.project_green.entity.Member;
+
+public interface MemberInfoService {
+
+    MemberDTO get(Long id);
+
+    void quit(Long id);
+
+    default Member dtoToEntity(MemberDTO dto){
+        Member member=Member.builder()
+                .id(dto.getId())
+                .password(dto.getPassword())
+                .email(dto.getEmail())
+                .build();
+        return member;
+    }
+
+    default MemberDTO entityToDTO(Member member){
+        MemberDTO memberDTO = MemberDTO.builder()
+                .id(member.getId())
+                .password(member.getPassword())
+                .email(member.getEmail())
+                .regDate(member.getRegDate())
+                .modDate(member.getModDate())
+                .build();
+        return memberDTO;
+
+
+    }
 }
--- src/main/java/com/group3/project_green/Service/MemberInfoServiceImpl.java
+++ src/main/java/com/group3/project_green/Service/MemberInfoServiceImpl.java
@@ -1,2 +1,26 @@
-package com.group3.project_green.Service;public class MemberInfoServiceImpl {
+package com.group3.project_green.Service;
+
+import com.group3.project_green.DTO.MemberDTO;
+import com.group3.project_green.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class MemberInfoServiceImpl implements MemberInfoService{
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public MemberDTO get(Long id) {
+        MemberDTO result = entityToDTO(memberRepository.getById(id));
+        return result;
+    }
+
+    @Override
+    public void quit(Long id){
+        memberRepository.deleteById(id);
+    }
 }
