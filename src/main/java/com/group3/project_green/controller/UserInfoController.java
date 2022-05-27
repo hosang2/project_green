f728d311b63b85751f5e2ede21e852061340af1e
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-27 16:16:19 +0900

    FEAT: 내정보 조회, 수정 목업 작성

--- src/main/java/com/group3/project_green/controller/UserInfoController.java
+++ src/main/java/com/group3/project_green/controller/UserInfoController.java
@@ -1,19 +1,45 @@
 package com.group3.project_green.controller;
 
+import com.group3.project_green.DTO.MemberDTO;
+import com.group3.project_green.Service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Log4j2
 @Controller
 @RequestMapping("/userinfo/*")
 @RequiredArgsConstructor
 public class UserInfoController {
 
+    private final MemberService memberService;
 
     @GetMapping("/userDetail")
-    void goUserInfo(){
+    void goUserInfo(Model model){
+        log.info("==================(Get) userDetail===============");
 
+        /* 임시로 1번 계정에만 접근합니다.
+        * 시큐리티 적용이 완료되면, 아래 코드에서 1L만 세션유저의 ID로 바꿔주면 됩니다.
+        * */
+        MemberDTO memberDTO = memberService.get(1L);
+
+        log.info("=================================memberDTO : " + memberDTO.toString());
+
+        model.addAttribute("userDetail", memberDTO);
+    }
+
+    @GetMapping("/userDetailModify")
+    void goUserInfoModify(Model model){
+        log.info("==================(Get) userDetailModify===============");
+
+        MemberDTO memberDTO = memberService.get(1L);
+
+        log.info("=================================memberDTO : " + memberDTO.toString());
+
+        model.addAttribute("userDetail", memberDTO);
     }
 
 }
