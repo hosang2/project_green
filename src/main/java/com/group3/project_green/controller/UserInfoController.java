0e8d1ee5d2d5acbb1f2a71fe81bce3d2a8831f95
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-27 22:06:50 +0900

    DOCS: 회원정보 컨트롤러, view 수정

--- src/main/java/com/group3/project_green/controller/UserInfoController.java
+++ src/main/java/com/group3/project_green/controller/UserInfoController.java
@@ -1,12 +1,14 @@
 package com.group3.project_green.controller;
 
 import com.group3.project_green.DTO.MemberDTO;
+import com.group3.project_green.Service.MemberInfoService;
 import com.group3.project_green.Service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class UserInfoController {
 
-    private final MemberService memberService;
+    private final MemberInfoService memberInfoService;
 
     @GetMapping("/userDetail")
     void goUserInfo(Model model){
@@ -24,7 +26,7 @@ public class UserInfoController {
         /* 임시로 1번 계정에만 접근합니다.
         * 시큐리티 적용이 완료되면, 아래 코드에서 1L만 세션유저의 ID로 바꿔주면 됩니다.
         * */
-        MemberDTO memberDTO = memberService.get(1L);
+        MemberDTO memberDTO = memberInfoService.get(1L);
 
         log.info("=================================memberDTO : " + memberDTO.toString());
 
@@ -35,11 +37,19 @@ public class UserInfoController {
     void goUserInfoModify(Model model){
         log.info("==================(Get) userDetailModify===============");
 
-        MemberDTO memberDTO = memberService.get(1L);
+        MemberDTO memberDTO = memberInfoService.get(1L);
 
         log.info("=================================memberDTO : " + memberDTO.toString());
 
         model.addAttribute("userDetail", memberDTO);
+    }
+
+    @PostMapping("/quit")
+    String quit(long i){
+        log.info("==================(Post) quit===============");
+        memberInfoService.quit(i);
+
+        return "redirect:/home/login";
     }
 
 }
