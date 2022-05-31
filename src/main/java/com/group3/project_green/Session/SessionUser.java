5b521ae71cc5188fa18c4c07dcfcee33235d7d46
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-31 23:35:59 +0900

    FEAT: 구글 로그인 기능 최종 추가

--- src/main/java/com/group3/project_green/Session/SessionUser.java
+++ src/main/java/com/group3/project_green/Session/SessionUser.java
@@ -4,19 +4,40 @@ import com.group3.project_green.entity.Member;
 import com.group3.project_green.entity.MemberInfo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import javax.persistence.Column;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Map;
+
 @Getter
-public class SessionUser implements Serializable {
+public class SessionUser extends User implements OAuth2User,Serializable {
     private Long id;
     private String password;
     private String email;
+    private Map<String, Object> attr;
 
-    public SessionUser(Member member) {
+    public SessionUser(Member member,
+                       Collection<? extends GrantedAuthority> authorities,
+                       Map<String, Object> attr) {
+        super(member.getEmail(), member.getPassword(), authorities);
         this.id = member.getId();
         this.email = member.getEmail();
         this.password = member.getPassword();
+        this.attr = attr;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attr;
+    }
+
+    @Override
+    public String getName() {
+        return email;
     }
 }
--- src/main/java/com/group3/project_green/security/SecurityTestController.java
+++ src/main/java/com/group3/project_green/security/SecurityTestController.java
@@ -1,2 +1,25 @@
-package com.group3.project_green.security;public class SecurityTestController {
+package com.group3.project_green.security;
+
+
+import com.group3.project_green.Session.SessionUser;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sequrity/")
+public class SecurityTestController {
+
+    @GetMapping("/test")
+    public void justMemberPrintTest(@AuthenticationPrincipal SessionUser sessionUser){
+
+        System.out.println("==================sessionUser : " + sessionUser.toString());
+        System.out.println("==================email : " + sessionUser.getEmail());
+        System.out.println("==================password : " + sessionUser.getPassword());
+        System.out.println("==================attr : " + sessionUser.getAttr().toString());
+
+    }
+
 }
