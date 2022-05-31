48135de6e9861eec2f0c0514911d7a209cdad705
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-31 23:35:40 +0900

    FEAD: 구글 로그인 기능 추가

--- src/main/java/com/group3/project_green/security/CustomOAuth2UserService.java
+++ src/main/java/com/group3/project_green/security/CustomOAuth2UserService.java
@@ -1,2 +1,72 @@
-package com.group3.project_green.security;public class CustomOAuth2UserService {
+package com.group3.project_green.security;
+
+import com.group3.project_green.Session.SessionUser;
+import com.group3.project_green.entity.Member;
+import com.group3.project_green.entity.MemberRole;
+import com.group3.project_green.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final MemberRepository memberRepository;
+    private final HttpSession httpSession;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String email = oAuth2User.getAttribute("email");
+        String password = passwordEncoder.encode("1111");
+
+        Member user = saveOrUpdate(email, password);
+
+        SessionUser sessionUser = new SessionUser(user,
+                user.getRoleSet().stream().map(
+                        role -> new SimpleGrantedAuthority("ROLE_"+role.name()))
+                .collect(Collectors.toList()),
+                oAuth2User.getAttributes());
+
+        httpSession.setAttribute("user", sessionUser);
+
+        return sessionUser;
+    }
+
+    public Member saveOrUpdate(String email, String password){
+
+        Optional<Member> user = memberRepository.findByEmail(email);
+
+        if(user.isPresent()){
+
+            return user.get();
+
+        } else {
+
+            Member member = Member.builder()
+                    .email(email)
+                    .password(password)
+                    .build();
+            member.addMemberRole(MemberRole.USER);
+
+            return memberRepository.save(member);
+        }
+
+    }
 }
--- src/main/java/com/group3/project_green/security/SecurityConfig.java
+++ src/main/java/com/group3/project_green/security/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.group3.project_green.security;
 
-public class SecurityConfig {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Log4j2
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)   // preAuthorize를 사용하기 위함임
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.formLogin().loginPage("/home/login");  // 로그인 페이지는 /home/login 입니다.
+        http.authorizeRequests().antMatchers("/home/**", "/newCSS/**").permitAll();
+        http.oauth2Login();
+    }
+
+    // PasswordEncoder로 BCryptPasswordEncoder를 사용
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
 }
