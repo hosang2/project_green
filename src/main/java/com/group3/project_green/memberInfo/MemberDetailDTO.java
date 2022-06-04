317c8daf3c8e349bc50bf7af81211d505c15f422
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-06-04 19:51:32 +0900

    FEAT: 회원정보 DTO 추가

--- src/main/java/com/group3/project_green/memberInfo/MemberDetailDTO.java
+++ src/main/java/com/group3/project_green/memberInfo/MemberDetailDTO.java
@@ -1,2 +1,32 @@
-package com.group3.project_green.memberInfo;public class MemberDetailDTO {
+package com.group3.project_green.memberInfo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.annotation.security.DenyAll;
+import javax.persistence.Id;
+
+@Data
+@Builder
+public class MemberDetailDTO {
+    private Long ino;
+
+    private String name;
+
+    private String phone;
+
+    private String birthDate;
+
+    private String address;
+
+    private String detailAddress;
+
+    private String location;
+
+    private String favoriteFood;
+    private String favoriteAccom;
+    private String favoriteLocation;
+
 }
