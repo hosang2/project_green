184c32dc2d92c39eec823e7b15ef01bb24e9167b
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-06-04 19:52:00 +0900

    FEAT: 회원 정보 관련 쿼리 추가

--- src/main/java/com/group3/project_green/memberInfo/repository/InfoMemberDetailRepository.java
+++ src/main/java/com/group3/project_green/memberInfo/repository/InfoMemberDetailRepository.java
@@ -1,2 +1,16 @@
-package com.group3.project_green.memberInfo.repository;public interface InfoMemberDetailRepository {
+package com.group3.project_green.memberInfo.repository;
+
+import com.group3.project_green.entity.Member;
+import com.group3.project_green.entity.MemberInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+public interface InfoMemberDetailRepository extends JpaRepository<MemberInfo, Long> {
+
+    @Transactional
+    Optional<MemberInfo> findByMemberId(Long id);
+
 }
--- src/main/java/com/group3/project_green/memberInfo/repository/InfoMemberRepository.java
+++ src/main/java/com/group3/project_green/memberInfo/repository/InfoMemberRepository.java
@@ -1,8 +1,11 @@
 package com.group3.project_green.memberInfo.repository;
 
 import com.group3.project_green.entity.Member;
+import com.group3.project_green.entity.MemberInfo;
 import com.group3.project_green.repository.MemberRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InfoMemberRepository extends JpaRepository<Member, Long> {
+
 }
